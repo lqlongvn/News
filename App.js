7f7717f5788085527a1c702b5f0b280d670723cc
Le Quoc Long <lqlongvn@gmail.com>
2020-08-14 11:32:47 +0700

    Hien thi Flatlist

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 
 import React from 'react';
 import {
+  Image,
+  FlatList,
   SafeAreaView,
   StyleSheet,
   ScrollView,
@@ -14,56 +16,84 @@ import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
+const DATA = [
+  {
+    id: '1',
+    iconAvatar: require("./myPic/exit.jpg"),
+    title: 'First Item',
+  },
+  {
+    id: '2',
+    iconAvatar: require("./myPic/exit_1.png"),
+    title: 'Second Item',
+  },
+  {
+    id: '3',
+    iconAvatar: require("./myPic/see_detail.png"),
+    title: 'Third Item',
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const renderItem = ({ item }) => (
+  <View>
+  <Image source={item.iconAvatar} style={styles.avatarStory} />
+  <Item title={item.title} />
+  </View>
+);
+
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
+      <Text style={[styles.header_text, { fontWeight: "bold" }]}>Worldwide News</Text>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-        </ScrollView>
-      </SafeAreaView>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+         </ScrollView>
+            </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  header_text: {
+    color: "#e85154",
+    fontSize: 32,
+    lineHeight: 40,
+    textAlign: "center",
+  },
+
   scrollView: {
     backgroundColor: Colors.lighter,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  body: {
-    backgroundColor: Colors.white,
+  title: {
+    fontSize: 32,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  avatarStory: {
+    width: 50,
+    height: 50,
   },
 });
 
