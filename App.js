64ca042a365ee6fd56b0f602acd0c883c5897adf
Le Quoc Long <lqlongvn@gmail.com>
2020-08-15 18:32:55 +0700

    Component Data

--- App.js
+++ App.js
@@ -16,26 +16,27 @@ import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
-const DATA = [
-  {
-    id: '1',
-    iconAvatar: require("./myPic/baloon_1.jpg"),
-    title: 'First News',
-    publishingTime:'15/8/20',
-  },
-  {
-    id: '2',
-    iconAvatar: require("./myPic/dapTamHiep.jpg"),
-    title: 'Second News',
-    publishingTime:'12/8/20',
-  },
-  {
-    id: '3',
-    iconAvatar: require("./myPic/helicopterUSA_1.jpg"),
-    title: 'Third News',
-    publishingTime:'14/8/20',
-  },
-];
+import { DATA } from "./myModules/newsData";
+// const DATA = [
+//   {
+//     id: '1',
+//     iconAvatar: require("./myPic/baloon_1.jpg"),
+//     title: 'First News',
+//     publishingTime:'15/8/20',
+//   },
+//   {
+//     id: '2',
+//     iconAvatar: require("./myPic/dapTamHiep.jpg"),
+//     title: 'Second News',
+//     publishingTime:'12/8/20',
+//   },
+//   {
+//     id: '3',
+//     iconAvatar: require("./myPic/helicopterUSA_1.jpg"),
+//     title: 'Third News',
+//     publishingTime:'14/8/20',
+//   },
+// ];
 
 const Item = ({ title }) => (
   <View style={styles.item}>
@@ -56,16 +57,12 @@ const App: () => React$Node = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Text style={[styles.header_text, { fontWeight: "bold" }]}>Worldwide News</Text>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+        <Text style={[styles.header_text, { fontWeight: "bold" }]}>Worldwide News 1</Text>
           <FlatList
             data={DATA}
             renderItem={renderItem}
             keyExtractor={item => item.id}
           />
-        </ScrollView>
       </SafeAreaView>
     </>
   );
---
+++ myModules/newsData.js
@@ -0,0 +1,20 @@
+export const DATA = [
+  {
+    id: '1',
+    iconAvatar: require("../myPic/baloon_1.jpg"),
+    title: 'First News',
+    publishingTime:'15/8/20',
+  },
+  {
+    id: '2',
+    iconAvatar: require("../myPic/dapTamHiep.jpg"),
+    title: 'Second News',
+    publishingTime:'12/8/20',
+  },
+  {
+    id: '3',
+    iconAvatar: require("../myPic/helicopterUSA_1.jpg"),
+    title: 'Third News',
+    publishingTime:'14/8/20',
+  },
+];
