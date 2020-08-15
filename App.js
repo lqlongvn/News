413b7b0d2fe2c81a2b1158df729da399684a6fc9
Le Quoc Long <lqlongvn@gmail.com>
2020-08-15 11:10:12 +0700

    picture, news and publishing date

--- App.js
+++ App.js
@@ -21,29 +21,33 @@ const DATA = [
     id: '1',
     iconAvatar: require("./myPic/baloon_1.jpg"),
     title: 'First News',
+    publishingTime:'15/8/20',
   },
   {
     id: '2',
     iconAvatar: require("./myPic/dapTamHiep.jpg"),
     title: 'Second News',
+    publishingTime:'12/8/20',
   },
   {
     id: '3',
     iconAvatar: require("./myPic/helicopterUSA_1.jpg"),
     title: 'Third News',
+    publishingTime:'14/8/20',
   },
 ];
 
 const Item = ({ title }) => (
   <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
+    <Text style={styles.titleStyle}>{title}</Text>
   </View>
 );
 
 const renderItem = ({ item }) => (
-  <View>
+  <View style={styles.newsList} >
     <Image source={item.iconAvatar} style={styles.avatarStory} />
     <Item title={item.title} />
+    <Item title={item.publishingTime} />
   </View>
 );
 
@@ -72,6 +76,9 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
   },
+  newsList:{
+    flexDirection: "column",
+  },
   header_text: {
     color: "#e85154",
     fontSize: 32,
@@ -88,12 +95,14 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     marginHorizontal: 16,
   },
-  title: {
+  titleStyle: {
     fontSize: 20,
+    width: "75%",
   },
   avatarStory: {
     width: 100,
     height: 100,
+    width: "25%",
   },
 });
 
