4adf6739544dc43dabefea9c7fe64fc90fe27d65
Le Quoc Long <lqlongvn@gmail.com>
2020-08-22 19:43:58 +0700

    Code thay Huy 18/8/20

--- Screen/Home(2).js
+++ Screen/Home(2).js
@@ -5,20 +5,12 @@ import {
   View,
   StyleSheet,
   Image,
+  Button,
   ActivityIndicator,
-  TouchableOpacity,
-  Linking,
-  Alert,
+  SafeAreaView,
 } from 'react-native';
 
-// https://github.com/oblador/react-native-vector-icons
-import Icon from 'react-native-vector-icons/FontAwesome5';
-
-// https://momentjs.com/
-import moment from 'moment';
-
 // Thay bằng key cá nhân để không bị hạn chế https://newsapi.org/
-//const API_KEY = 'ce52845d6e754123b3ecf9f68006b846'; //Key của thầy Huy
 const API_KEY = '2b31ab63402b46388c46ae1559570030'; //Key của Long
 
 const Home = () => {
@@ -26,20 +18,20 @@ const Home = () => {
   const [articles, setArticles] = useState([]);
   const [page, setPage] = useState(1);
 
+  console.log('State', isLoading);
+
   // Chỉ gọi API lần đầu render component
   useEffect(() => {
-    async function getNews() {
-      console.log('GET DATA FROM PAGE: ', page);
+    console.log('Get Data from page: '+page);
       const response = await fetch(
-        `https://newsapi.org/v2/top-headlines?sources=bbc-news,cbc-news,nbc-news,fox-news,mtv-news=&page=${page}&pageSize=10&apiKey=${API_KEY}`,
-      );
+        'https://newsapi.org/v2/top-headlines?sources=bbc-news,cbc-news,nbc-news,fox-news,mtv-news=&page='+page+'&pageSize=3&apiKey=' + API_KEY     );
       const jsonData = await response.json();
-      setArticles(articles.concat(jsonData.articles));
+      setArticles(jsonData.articles);
 
       // Lấy dữ liệu xong thì tắt biểu tượng loading
       setLoading(false);
-    }
-
+    
+    console.log('Call API');
     getNews();
   }, [page]);
 
@@ -48,39 +40,29 @@ const Home = () => {
       <Image source={{uri: item.urlToImage}} style={styles.thumbnail} />
 
       <View style={styles.info}>
-        <TouchableOpacity onPress={() => openArticle(item.url)}>
-          <Text style={styles.title} numberOfLines={3}>
-            {item.title}
-          </Text>
-        </TouchableOpacity>
-
-        <Text style={styles.publishedAt}>
-          <Icon name="clock" size={30} color="#000" />{' '}
-          {moment(item.publishedAt).fromNow()}
-        </Text>
+        <Text style={styles.newsContent}>{item.title}</Text>
+        <Text style={styles.publishedAt}>{item.publishedAt}</Text>
       </View>
     </View>
   );
 
   const renderSeparator = () => <View style={styles.separator} />;
 
-  async function loadMoreArticles() {
-    setPage((page) => page + 1);
+  async function loadMoreArticles(){
+    //Gọi lên api để lấy dữ liệu trang tiếp theo
+    setPage((page)=>page+1);
   }
 
-  async function openArticle(url) {
-    const supported = await Linking.canOpenURL(url);
-
-    if (supported) {
-      await Linking.openURL(url);
-    } else {
-      Alert.alert(`Don't know how to open this URL: ${url}`);
-    }
+  async function openArticle(){
+    //Gọi lên api để lấy dữ liệu trang tiếp theo
+    setPage((page)=>page+1);
   }
+
+
 
   return (
-    <View style={styles.container}>
-      <Text>News</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={[styles.header_text, { fontWeight: "bold" }]}>Worldwide News</Text>
 
       {isLoading ? (
         <ActivityIndicator size="small" color="#0000ff" />
@@ -91,11 +73,11 @@ const Home = () => {
           renderItem={renderItem}
           ItemSeparatorComponent={renderSeparator}
           keyExtractor={(item) => item.url}
+          onEndReachedThreshold={3}
           onEndReached={loadMoreArticles}
-          onEndReachedThreshold={1}
         />
       )}
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -103,6 +85,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 15,
+  },
+  header_text: {
+    color: "#e85154",
+    fontSize: 32,
+    lineHeight: 40,
+    textAlign: "center",
+    marginTop:15,
+    marginBottom:15,
   },
   list: {
     flex: 1,
@@ -116,19 +106,22 @@ const styles = StyleSheet.create({
     height: 150,
     resizeMode: 'contain',
   },
+  info:{
+    paddingHorizontal: 15,
+    paddingVertical: 25,
+    paddingRight: 15,
+  },
+  newsContent:{
+    fontSize:20,
+    fontWeight:"bold",
+    color:"blue",
+    resizeMode: 'contain',
+  },
   separator: {
     borderBottomWidth: 1,
     borderBottomColor: 'red',
     marginVertical: 10,
   },
-  info: {
-    flex: 1,
-    padding: 15,
-  },
-  title: {
-    marginBottom: 15,
-  },
-  publishedAt: {},
 });
 
 export default Home;
