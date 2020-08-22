4fb778edf1df307234d635ebda0632ccbb15e7a8
Le Quoc Long <lqlongvn@gmail.com>
2020-08-22 20:35:38 +0700

    Stack (1)

---
+++ Screen/Detail.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  FlatList,
+  View,
+  StyleSheet,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+  Linking,
+  Alert,
+} from 'react-native';
+
+  return (
+    <View style={styles.container}>
+      <Text> List </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 15,
+  },
+
+});
+
+export default Home;
---
+++ Screen/Login.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  FlatList,
+  View,
+  StyleSheet,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+  Linking,
+  Alert,
+} from 'react-native';
+
+  return (
+    <View style={styles.container}>
+      <Text> Login </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 15,
+  },
+
+});
+
+export default Home;
